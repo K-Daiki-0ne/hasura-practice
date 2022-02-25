2536be74c123204ee0c2471a9a34903949fc5759
_ 0ne <kouguchi3456@yahoo.co.jp>
2022-02-26 00:14:01 +0900

    delete: do not use code

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,5 @@
 import { ReactNode, VFC } from 'react';
 import Head from 'next/head';
-import Link from 'next/link';
 import Image from 'next/image';
 import { HeaderLink } from '../HeaderLink/HeaderLink';
 
